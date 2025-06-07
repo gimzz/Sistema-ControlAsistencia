23cc43053d605744f622b39b4271df8e9b5296dd
gimzz <letig732@gmail.com>
2025-06-07 13:51:13 -0400

    Feat: Se crea la interfaz de usuario, usando el patron DAO

---
+++ src/main/dao/UsuarioDAO.java
@@ -0,0 +1,12 @@
+package com.miempresa.gestionasistencia.dao;
+
+import com.miempresa.gestionasistencia.model.Usuario;
+import java.util.List;
+
+public interface UsuarioDAO {
+    boolean agregarUsuario(Usuario usuario);
+    Usuario obtenerUsuarioPorId(int id);
+    List<Usuario> obtenerTodosUsuarios();
+    boolean actualizarUsuario(Usuario usuario);
+    boolean eliminarUsuario(int id);
+}
