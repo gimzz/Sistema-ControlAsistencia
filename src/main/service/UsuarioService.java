f540b5fc93e38a9415dcb8bf553ebc475c255852
gimzz <letig732@gmail.com>
2025-06-13 21:02:48 -0400

    feat: cambios varios

--- src/main/service/UsuarioService.java
+++ src/main/service/UsuarioService.java
@@ -6,50 +6,40 @@ import com.miempresa.gestionasistencia.model.Usuario;
 
 import java.util.List;
 
-import jdk.internal.classfile.instruction.ThrowInstruction;
-
 public class UsuarioService {
     private UsuarioDAO usuarioDAO = new UsuarioDAOImpl();
 
-
-    public boolean registrarUsuario(Usuario usuario){
-        if (usuario.getNombre()== null || usuario.getNombre().isEmpty()) {
+    public boolean registrarUsuario(Usuario usuario) {
+        if (usuario.getNombre() == null || usuario.getNombre().isEmpty()) {
             throw new IllegalArgumentException("FALLO AL REGISTRAR USUARIO");
-            
         }
-        return UsuarioDAO.agregarUsuario(usuario);
+        return usuarioDAO.agregarUsuario(usuario);
     }
-    
-    public Usuario obtenerUsuarioPorId(int id){
-        return usuarioDAO.obtenerUsuarioPorId(int id);
-    }
-    
-    public Usuario obtenerUsuarioPorNombre(String nombre){
-    if(nombre==null || nombre.isEmpty()){
-        throw new IllegalArgumentException("NO HAY REGISTROS");
-    }
-    return usuarioDAO.obtenerUsuarioPorNombre(nombre);
 
-    
-    } 
-    
-    public List<Usuario> obtenerTodosUsuarios(){
+    public Usuario obtenerUsuarioPorId(int id) {
+        return usuarioDAO.obtenerUsuarioPorId(id);
+    }
+
+    public Usuario obtenerUsuarioPorNombre(String nombre) {
+        if (nombre == null || nombre.isEmpty()) {
+            throw new IllegalArgumentException("NO HAY REGISTROS");
+        }
+        return usuarioDAO.obtenerUsuarioPorNombre(nombre);
+    }
+
+    public List<Usuario> obtenerTodosUsuarios() {
         return usuarioDAO.obtenerTodosUsuarios();
     }
-    
-    public boolean actualizarUsuario(Usuario usuario){
-    if(usuario.getNombre()==null || usuario.getNombre().isEmpty()){
-     throw new IllegalArgumentException("ERROR PARA ACTUALIZAR");
 
-    }
-        try {
-         return usuarioDAO.actualizarUsuario(usuario);
-        } catch(Exception e){
-        e.printStackTrace();
-        return false;
+    public boolean actualizarUsuario(Usuario usuario) {
+        if (usuario.getNombre() == null || usuario.getNombre().isEmpty()) {
+            throw new IllegalArgumentException("ERROR PARA ACTUALIZAR");
         }
-    
+        try {
+            return usuarioDAO.actualizarUsuario(usuario);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
-
-    
 }
